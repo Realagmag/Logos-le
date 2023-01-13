79b2a2fa40cdf925f4da6ef844ed2daff0cc81a3
Konrad Karpiuk <01178228@pw.edu.pl>
2023-01-13 17:23:58 +0100

    created get_random_word() function

--- classes.py
+++ classes.py
@@ -1,2 +1,18 @@
+from random import choice
+
+
+def get_random_word():
+    with open("all_words.txt", "r") as file_handle:
+        list_of_words = []
+        for line in file_handle:
+            list_of_words.append(line)
+        word_to_guess = choice(list_of_words)
+        return Word(word_to_guess)
+
+
 class Word():
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return self.name
